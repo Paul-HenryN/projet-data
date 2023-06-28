6da2042fde433627ef0f33a2f6b00df3aaca6f3a
Paul-HenryN <paul-henry.ngankam@2025.ucac-icam.com>
2023-06-28 23:04:49 +0100

    comment code

--- code/vrp_genetic_solver.py
+++ code/vrp_genetic_solver.py
@@ -3,56 +3,52 @@ import networkx as nx
 import matplotlib.pyplot as plt
 import random
 
+# Create a test graph
 graph = nx.complete_graph(10)
+# Add random weights
 nx.set_edge_attributes(
     graph, {e: {"weight": random.randint(1, 10)} for e in graph.edges}
 )
 
+# Extract adjacency matrix of the graph
 matrix = nx.adjacency_matrix(graph).todense()
-
-# Définition du problème
-distances = matrix
-n = len(distances)  # Nombre de villes
+n = len(matrix)  # Number of nodes (cities)
 
 
-def evaluate_tour(tour):
+# Calculate the cost of a tour
+def cost(tour):
     total_distance = 0
     for i in range(n - 1):
-        total_distance += distances[tour[i]][tour[i + 1]]
-    total_distance += distances[tour[-1]][tour[0]]  # Retour à la première ville
+        total_distance += matrix[tour[i]][tour[i + 1]]
+    total_distance += matrix[tour[-1]][tour[0]]  # Retour à la première ville
     return total_distance
 
 
-# Algorithme génétique
+# Initialize a random population of solutions
 def initialize_population(population_size):
     population = []
     for _ in range(population_size):
         individual = list(range(n))
-        random.shuffle(individual)  # Permute aléatoirement l'ordre
+        random.shuffle(individual)
         population.append(individual)
     return population
 
 
-def select_parents(population):
+# Selection
+def selection(population):
     fitness_values = []
     for individual in population:
-        fitness_values.append(
-            1 / evaluate_tour(individual)
-        )  # Calcul de l'inverse de la distance parcourue
+        fitness_values.append(1 / cost(individual))
     sum_fitness = sum(fitness_values)
     probabilities = [fitness / sum_fitness for fitness in fitness_values]
-    parents = random.choices(
-        population, probabilities, k=2
-    )  # Sélection de deux parents
+    parents = random.choices(population, probabilities, k=2)
     return parents
 
 
 def crossover(parents):
     parent1, parent2 = parents
     child = [None] * n
-    start, end = sorted(
-        random.sample(range(n), 2)
-    )  # Choix de deux points de coupure pour le croisement
+    start, end = sorted(random.sample(range(n), 2))
     child[start:end] = parent1[start:end]
     remaining_cities = [city for city in parent2 if city not in child[start:end]]
     index = 0
@@ -65,12 +61,12 @@ def crossover(parents):
 
 def mutate(individual, mutation_rate):
     for i in range(n):
-        if random.random() < mutation_rate:  # Mutation avec une probabilité donnée
+        if random.random() < mutation_rate:
             j = random.randint(0, n - 1)
             individual[i], individual[j] = (
                 individual[j],
                 individual[i],
-            )  # Permutation de deux villes
+            )
     return individual
 
 
@@ -83,7 +79,7 @@ def genetic_algorithm(population_size, generations, mutation_rate):
         new_population = []
 
         while len(new_population) < population_size:
-            parents = select_parents(population)
+            parents = selection(population)
             child = crossover(parents)
             child = mutate(child, mutation_rate)
             new_population.append(child)
@@ -91,7 +87,7 @@ def genetic_algorithm(population_size, generations, mutation_rate):
         population = new_population
 
         for individual in population:
-            fitness = evaluate_tour(individual)
+            fitness = cost(individual)
             if fitness < best_fitness:
                 best_fitness = fitness
                 best_individual = individual
