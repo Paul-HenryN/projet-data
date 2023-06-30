f8c131e7cd07ecad464f528985c656f76a88be71
TANKISTE <Tankiste>
2023-06-30 03:01:20 +0100

    Update of the Statistical descriptions

--- statistics/scriptsR.R
+++ statistics/scriptsR.R
@@ -2,7 +2,7 @@ library(readxl)
 
 
 #Specifying the excel file location
-path <- "C:/Users/hp/Documents/X3/U.E_5_Traitement_de_données/Projet_DATA/projet-data/excelFile.xlsx"
+path <- "C:/Users/hp/Documents/X3/U.E_5_Traitement_de_données/Projet_DATA/projet-data/data/sample.xlsx"
 
 #Accessing the excel file using R 
 dataFile <- read_excel(path)
@@ -19,17 +19,35 @@ sd(Convergence_Time)
 
 
 #Création d'une liste avec les caractères à étudier
-#Puis on boucle sur chaque élément de la lsite afin d'afficher le :
-# Mode , Histogramme , Boîte à moustache
+#Puis on boucle sur chaque élément de la liste afin d'afficher le Mode
 lst <- list(Covered_dist, Convergence_Time)
 for (i in 1:length(lst)) {
   elmt <- table(lst[[i]])
   mode_function <- rownames(elmt)[which.max(elmt)]
   
   print(paste("Mode for element", i, ":", mode_function))
-  hist(x=i, main = "Histogram of Results", xlab = "Values")
-  boxplot(i, main ="Boxplot", ylab = "Valeurs", xlab = "Groupe")
 }
+
+#Draws a histogram of the convergence time then delays for 2 seconds
+hist(Convergence_Time, main = "Histogram of Convergence Time", xlab = "Values")
+Sys.sleep(2)
+
+#Draws a histogram of the covered distance then delays for 2 seconds
+hist(Covered_dist, main = "Histogram of Covered Distance", xlab = "Values")
+Sys.sleep(2)
+
+#Draws a mustache box of the Convergence Time features then delays for 2 seconds
+boxplot(Convergence_Time, main = "Boxplot Convergence Time", col = c("skyblue"), ylab = "Values", xlab = "Data")
+Sys.sleep(2)
+
+#Draws a mustache box of the Covered distance features
+boxplot(Covered_dist, main = "Boxplot Covered Distances", col = c("red"), ylab = "Values", xlab = "Data")
+
+
+
+
+
+
 
 
 
